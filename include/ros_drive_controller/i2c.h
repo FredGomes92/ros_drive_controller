fd2a9d60dda8ce18416bcfd632482d4092d19875
FredGomes92 <Fred.p.gomes92@gmail.com>
2021-03-30 18:38:33 +0000

    Hardware draft

--- include/ros_drive_controller/i2c.h
+++ include/ros_drive_controller/i2c.h
@@ -4,18 +4,23 @@
 #include <fcntl.h>
 #include <errno.h>
 
-class I2C
+namespace i2c
 {
-    public:
-        I2C(int, int);
-        virtual ~I2C();
-        uint8_t readBytes(uint8_t* buff,uint8_t bufferSize);
-        int writeData(uint8_t* data,uint8_t dataSize);
-    private:
-        int _i2caddr;
-        int _i2cbus;
-        void openfd();
-        char busfile[64];
-        int fd;
-};
+
+    class I2C
+    {
+        public:
+            I2C(int, int);
+            virtual ~I2C();
+            uint8_t readBytes(uint8_t* buff,uint8_t bufferSize);
+            int writeData(uint8_t* data,uint8_t dataSize);
+        private:
+            int _i2caddr;
+            int _i2cbus;
+            void openfd();
+            char busfile[64];
+            int fd;
+            int motor_prev_cmd = 0;
+    };
+}
 
--- src/motor_driver.cpp
+++ src/motor_driver.cpp
@@ -1,5 +1,25 @@
 #include <ros/ros.h>
 #include "ros_drive_controller/i2c.h"
+#include <string>
+
+using namespace std;
+using namespace i2c;
+
+void GetResponse(string &ss, I2C *motor)
+{
+    uint8_t buff[3] = {0};
+
+    motor->readBytes(buff, 5);
+
+    ROS_INFO("Data received: ");
+
+    for (int i = 0; i < 5; i++)
+    {
+        ss += buff[i];
+
+        ROS_INFO("Byte received : %d", buff[i]);
+    }
+}
 
 int main(int argc, char ** argv)
 {
@@ -7,21 +27,37 @@ int main(int argc, char ** argv)
 
     ros::NodeHandle _nh;
 
-
     I2C motor(1, 10); // Bus 1 , address 10
 
-    uint8_t buff[1] = {10};
+    string ss;
+
+    int vel = 300; // º/s
+    int pos = 102;
+
+    uint8_t buff[4];
+
+    buff[0] = vel >> 8; //MSB
+    buff[1] = vel; // LSB
+    buff[2] = pos >> 8; // MSB
+    buff[3] = pos; // LSB
+
+    ROS_INFO ("I2c master has started!");
 
     while( ros::ok())
     {
+        ROS_INFO("Writting data ...");
+        ROS_INFO("vel: buff[0]: %d, buff[1]= %d", buff[0], buff[1]);
+        ROS_INFO("pos: buff[2]: %d, buff[3]= %d", buff[2], buff[3]);
 
-        motor.writeData(buff,1);
+        motor.writeData(buff,4);
 
         ros::Duration(2).sleep();
+
+        // GetResponse(ss, &motor);
+
+        ROS_INFO("Received data: %s", ss);
 
     }
 
     return 0;
-
-
 }
